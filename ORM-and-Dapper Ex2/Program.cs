55fca7bc23973f312723fece7608359f05b7075e
Tom Settle <tom.k.settle@gmail.com>
2023-02-28 13:02:14 -0600

    Empty solution to start with

---
+++ ORM-and-Dapper Ex2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ORM_and_Dapper_Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
